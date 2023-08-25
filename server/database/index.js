ff50c6168e20c99659d394ef3f81b7ecc0d8f4e7
mcjhassenboehler <Mcjhassenboehler@gmail.com>
2023-08-25 15:10:14 -0500

    server/db/index work plus sosme extra

--- server/database/index.js
+++ server/database/index.js
@@ -6,7 +6,7 @@ const DATABASE = 'fetcher';
 const DB_URI = `mongodb://localhost/${DATABASE}`;
 // TODO: Put the field you are gonna use to sort the repos by.
 // Your schema should have this field.
-const SORTING_BY_FIELD = '';
+const SORTING_BY_FIELD = '_id';
 
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to database'))
@@ -14,18 +14,41 @@ mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const repoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  _id: Number,
+  name: String,
+  owner: String,
+  owner_avatar: String,
+  owner_url: String,
+  html_url: String,
+  updated_at: Date,
+  description: String
 });
 
 const Repo = mongoose.model('Repo', repoSchema);
 
-const saveRepo = () => {
+const saveRepo = (repos, callback) => {
   // TODO: Your code here
   // This function should save a repo to the MongoDB
+    Repo.insertMany(repos, {ordered: false}, (err, data) => {
+      if (err){
+        callback(err)
+      } else {
+        callback(null, data)
+      }
+    })
+
 };
 
-const getTop25Repos = () => {
+const getTop25Repos = (callback) => {
   // TODO: Your code here
   // This function should get the repos from mongo
+  Repo.find({}, {}, {limit: 25}, (err, data) => {
+    if (err){
+      callback(err)
+    } else {
+      callback(null, data)
+    }
+  }).sort('-updated_at')
 };
 
 module.exports.SORTING_BY_FIELD = SORTING_BY_FIELD;
--- server/helpers/github.js
+++ server/helpers/github.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 const config = require('../config');
 const testData = require('../data.json');
 
-const getReposByUsername = () => {
+const getReposByUsername = (username, callback) => {
   // TODO: Use the axios module to get repos for a specific user from the github API
 
   // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   const options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       // https://developer.github.com/v3/#user-agent-required
       'User-Agent': 'request',
@@ -17,6 +17,13 @@ const getReposByUsername = () => {
     },
   };
 
+  axios.get(options.url, { headers: options.headers})
+  .then((response) => {
+    callback(null, response.data)
+  })
+  .catch((err) => {
+    callback(err)
+  })
 };
 
 module.exports.getReposByUsername = getReposByUsername;
